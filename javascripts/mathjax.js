e4a35d184f7bd77a1dbb10fda5c8d95ec023cb8b
pcahan1 <patrick.cahan@gmail.com>
2026-02-24 16:22:05 -0500

    Deployed f36495e with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    processHtmlClass: "arithmatex|jp-RenderedHTMLCommon|jp-RenderedMarkdown"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
+
+
